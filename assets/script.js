c5d14b202bcdb9613a4bde24aad4e17410564aa4
atengelsgjerd <atengelsgjerd@gmail.com>
2024-03-25 21:47:50 -0500

    added code to store user blog info on first page in an object within the localStorage.

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,31 @@
+const buttonToggle = document.querySelector("toggle");
+
+const userNameInput = document.querySelector("#userName");
+const titleInput = document.querySelector("#title");
+const contentInput = document.querySelector("#content");
+
 const addSubmitButton = document.querySelector("#submit-button");
+
+addSubmitButton.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    const userObject = {
+        userName: userNameInput.value,
+        title: titleInput.value,
+        content: contentInput.value,
+    }
+    
+    localStorage.setItem("userObject", JSON.stringify(userObject));
+
+    let newObject = localStorage.getItem("userObject");
+    console.log(JSON.parse(newObject));
+})
+
+const dayNight = function(setting){
+    //This code will change the icon from day setting to night setting.
+}
+
+const handleToggle = function(){
+    //This code will handle the toggle from day to night.
+}
 
